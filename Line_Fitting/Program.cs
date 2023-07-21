ce701d982f844bc73ca573b941903877c144679d
fathug <1052009660@qq.com>
2023-07-21 17:01:40 +0800

    加入Ransac算法

--- Line_Fitting/Program.cs
+++ Line_Fitting/Program.cs
@@ -19,13 +19,15 @@ internal class Program
         Cv2.ImShow("灰度图像", grayImage);
         Cv2.WaitKey(0); //显示等待
         Cv2.DestroyAllWindows();
-
         Cv2.ImWrite(outputPath, grayImage);   //保存灰度图像
+
         List<Point> edgePixel = ImageUtils.GetPixelCoordinate(grayImage);
-        foreach (Point point in edgePixel)
+        foreach (Point point in edgePixel)  //打印所有的边缘像素
         {
-            Console.WriteLine(point);
+            Console.Write(point+" ");
         }
+        Console.WriteLine();
+
         ImageUtils.FitLine(originImage2, edgePixel);
         Cv2.NamedWindow("拟合图像", WindowFlags.Normal);
         Cv2.ImShow("拟合图像", originImage2);
@@ -48,6 +50,7 @@ internal static class ImageUtils
         Cv2.CvtColor(originImage, grayImage, ColorConversionCodes.BGR2GRAY);
         return grayImage;
     }
+
     /// <summary>
     /// 获取像素坐标
     /// </summary>
@@ -66,13 +69,74 @@ internal static class ImageUtils
         }
         return coordinate;
     }
+
     /// <summary>
     /// 拟合直线并画线
     /// </summary>
     public static void FitLine(Mat originImage, List<Point> coordinate) //变量1是原始图像，为了在上面画线；变量2是边缘坐标
     {
+        //使用ransac算法去除噪点
+        int numIterations = 10; // 迭代次数
+        double thresholdDistance = 2.0; //内点的阈值距离
+        int numPointsInLine = 2; // 用于拟合直线的最小内点数
+        double bestM = 0, bestB = 0;
+        int maxInliers = 0;
+
+        Random rand = new Random();
+        for (int i = 0; i < numIterations; i++)
+        {
+            // 随机选择两个点
+            int index1 = rand.Next(coordinate.Count);
+            int index2 = rand.Next(coordinate.Count);
+            if (index1 == index2)
+                continue;
+            Point p1 = coordinate[index1];
+            Point p2 = coordinate[index2];
+
+            // 计算直线参数
+            if ((p2.X - p1.X) == 0)     //判断分母是否为0
+            {
+                continue;
+            }
+            double m0 = (p2.Y - p1.Y) / (p2.X - p1.X);
+            double b0 = p1.Y - m0 * p1.X;
+
+            // 计算内点个数
+            int inliers = 0;
+            foreach (var point in coordinate)
+            {
+                double distance = Math.Abs(point.Y - (m0 * point.X + b0));
+
+                if (distance < thresholdDistance)
+                    inliers++;
+            }
+            if (inliers > maxInliers && inliers >= numPointsInLine)
+            {
+                maxInliers = inliers;
+                bestM = m0;
+                bestB = b0;
+            }
+        }
+
+        //创建新list保存合格点
+        List<Point> coordinate2 = new List<Point>();
+        foreach (var point in coordinate)
+        {
+            double distance = Math.Abs(point.Y - (bestM * point.X + bestB));
+            if (distance < thresholdDistance)
+            {
+                coordinate2.Add(point);
+            }
+        }
+        //打印去噪后的像素
+        Console.WriteLine("Ransac去噪后剩余坐标：");
+        foreach (Point point in coordinate2)
+        {
+            Console.Write(point+" ");
+        }
+
         //将Point转换为数组
-        int n = coordinate.Count; //Point的数量
+        int n = coordinate2.Count;
         double[] x = new double[n];
         double[] y = new double[n];
         for (int i = 0; i < n; i++)
